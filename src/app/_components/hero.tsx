4f5dce6c2138c2b58f3f0eb341bba78702161216
Patrik <tokapatrik321@gmail.com>
2024-01-04 12:53:34 +0100

    Add more meta data

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -5,7 +5,7 @@ import SocialIcons from '@/components/socialIcons'
 const Hero = () => {
     return (
         <section className="common-bg relative flex flex-col justify-center pb-32 pt-[14rem] md:h-screen md:min-h-[50rem] md:pt-16" id="fooldal">
-            <h1>Köszöntelek az oldalon!</h1>
+            <h1>Köszöntelek az oldalamon!</h1>
             <p className="mx-auto mb-8 max-w-3xl text-center text-lg text-solidblack md:text-2xl">
                 Tóka Patrik vagyok <span className="font-bold">Full Stack Fejlesztő</span>, weboldalak és webshopok fejlesztésével foglalkozom.
             </p>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,57 @@ const sourceSans = Source_Sans_3({
 })
 
 export const metadata: Metadata = {
+    metadataBase: new URL('https://tokapatrik.hu'),
     title: 'Tóka Patrik - Full Stack Fejlesztő',
-    description: 'Fedezd fel kreatív és innovatív webes projekteimet, ahol a technológia és dizájn találkozik. Nézd meg munkáimat és ismerj meg!'
+    description: 'Fedezd fel kreatív és innovatív webes projekteimet, ahol a technológia és dizájn találkozik. Nézd meg munkáimat és ismerj meg!',
+    creator: 'Tóka Patrik',
+    keywords: [
+        'Tóka Patrik',
+        'Next.js',
+        'React',
+        'JavaScript',
+        'Full Stack Fejlesztő',
+        'Webfejlesztés',
+        'Frontend Fejlesztés',
+        'Backend Fejlesztés',
+        'UI/UX Design',
+        'React Fejlesztés',
+        'Next.js Fejlesztés',
+        'Webalkalmazások',
+        'Optimalizálás',
+        'Egyedi Weboldalak',
+        'Portfólió',
+        'Teljesítmény optimalizálás',
+        'Modern webes technológiák',
+        'Kreatív webmegoldások',
+        'Webes projektek',
+        'TypeScript',
+        'React',
+        'Next',
+        'AWS',
+        'NodeJS',
+        'Express',
+        'GIT',
+        'Github',
+        'Docker',
+        'SQL/NoSQL databases',
+        'PHP',
+        'HTML',
+        'CSS',
+        'JavaScript'
+    ],
+    openGraph: {
+        type: 'website',
+        url: 'https://tokapatrik.hu',
+        title: 'Tóka Patrik - Full Stack Fejlesztő',
+        description: 'Fedezd fel kreatív és innovatív webes projekteimet, ahol a technológia és dizájn találkozik. Nézd meg munkáimat és ismerj meg!',
+        siteName: 'Tóka Patrik - Full Stack Fejlesztő',
+        images: [
+            {
+                url: 'https://tokapatrik.hu/og_image.webp'
+            }
+        ]
+    }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
