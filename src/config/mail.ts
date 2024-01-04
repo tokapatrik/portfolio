deda8d5a57cdd4a7a34a0d6df3c3fa0e45510eed
Patrik <tokapatrik321@gmail.com>
2024-01-04 09:00:30 +0100

    Environment varaibles rename

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,8 +1,8 @@
 import { SESClient, SESClientConfig } from '@aws-sdk/client-ses'
 
-const accessKeyId = process.env.AWS_SNS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SNS_SECRET_ACCESS_KEY
-const region = process.env.AWS_SES_REGION
+const accessKeyId = process.env.SES_ACCESS_KEY
+const secretAccessKey = process.env.SES_SECRET_ACCESS_KEY
+const region = process.env.SES_REGION
 
 if (!accessKeyId || !secretAccessKey || !region) {
     throw new Error('AWS SES credentials are not provided')
